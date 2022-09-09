f4094fad7759543f8d8aa726af6aee96b8b9745b
vinicius-escano <vini.e@outlook.com>
2022-09-09 15:00:50 -0300

    Criando lista de ordens de separação

--- src/main/java/com/separapecas/separapecas/SeparapecasApplication.java
+++ src/main/java/com/separapecas/separapecas/SeparapecasApplication.java
@@ -15,6 +15,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class SeparapecasApplication implements CommandLineRunner {
@@ -31,11 +32,16 @@ public class SeparapecasApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("***** INICIANDO ALGORITMO *****\nBuscando Dados");
+        System.out.println("\n\n***** INICIANDO ALGORITMO *****\nBuscando Dados");
         List<Produto> listProduto = new ArrayList<>();
         int randomNum;
-        for(int i  = 0; i < 10 ; i++) {
+        for (int i = 0; i < 10; i++) {
             randomNum = ThreadLocalRandom.current().nextInt(1, 35);
+            for(Produto p : listProduto){
+                while(p.getCodigo() == randomNum){
+                    randomNum = ThreadLocalRandom.current().nextInt(1, 35);;
+                }
+            }
             listProduto.add(produtoService.findById(randomNum));
         }
 
@@ -45,7 +51,9 @@ public class SeparapecasApplication implements CommandLineRunner {
         listaOrganizada.stream().forEach(p -> {
             System.out.println(p.getDescricao() + " / Localização : " + p.getLocalizacao());
         });
-        System.out.println("***** FINALIZADO COM SUCESSO *****");
+
+        System.out.println("\n_-* Roteirização *-_\n" + montaRota(listaOrganizada, prateleiras));
+        System.out.println("\n\n***** FINALIZADO COM SUCESSO *****\n\n");
     }
 
     public List<Produto> ordena(List<Produto> listProdutos, List<Prateleira> prateleiras) {
@@ -73,38 +81,100 @@ public class SeparapecasApplication implements CommandLineRunner {
                         produtosQ3.add(p);
                     } else if (pr.getQuadrante().equals(Quadrante.QUAD4)) {
                         produtosQ4.add(p);
-                    } else if (pr.getQuadrante().equals(Quadrante.QUAD5)){
+                    } else if (pr.getQuadrante().equals(Quadrante.QUAD5)) {
                         produtosQ5.add(p);
                     }
                 }
             });
         });
 
-        if(!produtosQ4.isEmpty()){
-            produtosQ4.sort(Comparator.comparingInt(Produto :: getPrateleiraCodigo));
+        if (!produtosQ4.isEmpty()) {
+            produtosQ4.sort(Comparator.comparingInt(Produto::getPrateleiraCodigo));
             listaOrdenada.addAll(produtosQ4);
         }
-        if(!produtosQ2.isEmpty()){
-            produtosQ2.sort(Collections.reverseOrder(Comparator.comparingInt(Produto :: getPrateleiraCodigo)));
+        if (!produtosQ2.isEmpty()) {
+            produtosQ2.sort(Collections.reverseOrder(Comparator.comparingInt(Produto::getPrateleiraCodigo)));
             listaOrdenada.addAll(produtosQ2);
         }
-        if(!produtosQ1.isEmpty()){
-            produtosQ1.sort(Collections.reverseOrder(Comparator.comparingInt(Produto :: getPrateleiraCodigo)));
+        if (!produtosQ1.isEmpty()) {
+            produtosQ1.sort(Collections.reverseOrder(Comparator.comparingInt(Produto::getPrateleiraCodigo)));
             listaOrdenada.addAll(produtosQ1);
         }
-        if(!produtosQ5.isEmpty()){
-            produtosQ5.sort(Collections.reverseOrder(Comparator.comparingInt(Produto :: getPrateleiraCodigo)));
+        if (!produtosQ5.isEmpty()) {
+            produtosQ5.sort(Collections.reverseOrder(Comparator.comparingInt(Produto::getPrateleiraCodigo)));
         }
-        if(!produtosQ3.isEmpty()){
-            produtosQ3.sort(Collections.reverseOrder(Comparator.comparingInt(Produto :: getPrateleiraCodigo)));
+        if (!produtosQ3.isEmpty()) {
+            produtosQ3.sort(Collections.reverseOrder(Comparator.comparingInt(Produto::getPrateleiraCodigo)));
             listaOrdenada.addAll(produtosQ3);
         }
         return listaOrdenada;
 
     }
-    
-    private String getPrateleira(String localizacao){
+
+    private String getPrateleira(String localizacao) {
         return localizacao.substring(0, localizacao.indexOf("-"));
+    }
+
+    private String montaRota(List<Produto> listProdutos, List<Prateleira> listPrateleiras) {
+        StringBuilder sb = new StringBuilder();
+        String continua = "Continue pelo corredor e dirija-se para a Prateleira ";
+        String retorna = "Retorne o corredor e dirija-se para a Prateleira ";
+        Integer posicaoAtual;
+        Integer posicaoProximo;
+        boolean direcao = true; // true -> subindo e false -> descendo
+        Prateleira pratAtual;
+        Prateleira pratProxima;
+        int i = 0;
+        for (i = 0; i < listProdutos.size(); i++) {
+            Produto atualProduto = listProdutos.get(i);
+            posicaoAtual = getPosicao(listProdutos.get(i));
+            if (i != listProdutos.size() - 1) {
+                posicaoProximo = getPosicao(listProdutos.get(i + 1));
+                Produto proxProduto = listProdutos.get(i + 1);
+                pratAtual = listPrateleiras.stream().filter(prat -> atualProduto.getPrateleiraCodigo() == prat.getCodigo()).collect(Collectors.toList()).get(0);
+                pratProxima = listPrateleiras.stream().filter(prat -> proxProduto.getPrateleiraCodigo() == prat.getCodigo()).collect(Collectors.toList()).get(0);
+                if (i == 0) {
+                    sb.append("Dirija-se a Prateleira ").append(getPrateleira(atualProduto.getLocalizacao()))
+                            .append(" Posição: ").append(posicaoAtual).append("\n");
+                }
+                double dif = ondeVou(posicaoAtual, posicaoProximo);
+                if(pratAtual.getQuadrante().equals(pratProxima.getQuadrante())) {
+                    if (ondeEstou(posicaoAtual) < 1) {
+                        if (dif > 1) {
+                            sb.append(continua);
+                        } else {
+                            sb.append(retorna);
+                        }
+                    } else if (dif < 1){
+                        if (dif > 1) {
+                            sb.append(continua);
+                        } else {
+                            sb.append(retorna);
+                        }
+                    } else {
+                        sb.append("Pegue o item da prateleira ");
+                    }
+                    sb.append(getPrateleira(proxProduto.getLocalizacao())).append(" Posição: ").append(posicaoProximo).append("\n");
+                } else {
+                    sb.append("Dirija-se a Prateleira ").append(getPrateleira(proxProduto.getLocalizacao())).append(" Posição: ").append(posicaoProximo).append("\n");
+                }
+            } else {
+                sb.append("Entregue os itens na conferência!");
+            }
+        }
+        return sb.toString();
+    }
+
+    private Integer getPosicao(Produto produto) {
+        return Integer.valueOf(produto.getLocalizacao().substring(produto.getLocalizacao().indexOf("-") + 1));
+    }
+
+    private double ondeVou(Integer posicaoAtual, Integer proximaPosicao) {
+        return proximaPosicao / posicaoAtual;
+    }
+
+    private double ondeEstou(Integer posicaoAtual) {
+        return posicaoAtual / 150;
     }
 
 }
